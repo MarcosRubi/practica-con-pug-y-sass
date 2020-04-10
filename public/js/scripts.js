b2d4fe355746b2da50122bf9f6ca596d83978f4c
Daniel Hernández <danielhernandez9980@gmail.com>
2020-04-10 17:53:26 -0600

    agregando header responsive

---
+++ public/js/scripts.js
@@ -0,0 +1,13 @@
+    $(document).ready(function(){
+        $("#menu ul li:nth-child(4)").click(function(){
+             $("#menu ul ul").css("display","block");
+             $("#menu ul ul").css("position","relative");
+        });
+        
+    });
+    function mobile() {
+        $( "nav.menu" ).toggleClass( "toggle" );
+    };
+    
+            
+
